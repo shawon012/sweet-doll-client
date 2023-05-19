fe41c930c7da2f38ae624402e79bd0eb8e5d9ddd
shawon012 <shawon.shakib01@gmail.com>
2023-05-19 12:03:19 +0600

    route set up done

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,54 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home/Home";
+import Login from "../Pages/Authorize/Login";
+import Signup from "../Pages/Authorize/Signup";
+import SingleToy from "../Toys/SingleToy/SingleToy";
+import AllToy from "../Toys/AllToys/AllToy";
+import AddaToy from "../Toys/AddaToy/AddaToy";
+import MyToys from "../Toys/MyToys/MyToys";
+import Notfound from "../Notfound/Notfound";
 
 
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Main></Main>,
+      children:[
+        {
+            path: '/',
+            element: <Home></Home>
+        },
+        {
+            path: 'login',
+            element: <Login></Login>
+        },
+        {
+            path: 'signUp',
+            element: <Signup></Signup>
+        },
+        {
+            path: 'products/:id',
+            element: <SingleToy></SingleToy>
+        },
+        {
+            path: 'products',
+            element: <AllToy></AllToy>
+        },
+        {
+            path: 'addProduct',
+            element: <AddaToy></AddaToy>
+        },
+        {
+            path: 'mytoys',
+            element: <MyToys></MyToys>
+        }
+      ]
     },
+    {
+        path: '*',
+        element: <Notfound></Notfound>
+    }
   ])
   export default router;
---
+++ src/Toys/AddaToy/AddaToy.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddaToy = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AddaToy;
